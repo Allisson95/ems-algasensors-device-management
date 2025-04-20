1f464ef422776db1341ab755912bd3b3dc3c0e15
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-04-20 14:05:42 -0300

    add TSID support for sensor retrieval in SensorController and implement StringToTSIDConverter

--- src/main/java/dev/allisson/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/dev/allisson/algasensors/device/management/api/controller/SensorController.java
@@ -4,14 +4,15 @@ import dev.allisson.algasensors.device.management.api.mapper.SensorMapper;
 import dev.allisson.algasensors.device.management.api.model.SensorInput;
 import dev.allisson.algasensors.device.management.api.model.SensorOutput;
 import dev.allisson.algasensors.device.management.domain.model.Sensor;
+import dev.allisson.algasensors.device.management.domain.model.SensorId;
 import dev.allisson.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 
@@ -23,6 +24,14 @@ public class SensorController {
 
     private final SensorRepository sensorRepository;
     private final SensorMapper sensorMapper;
+
+    @GetMapping("{sensorId}")
+    public ResponseEntity<SensorOutput> getOne(@PathVariable final TSID sensorId) {
+        log.info("Received a new request to get a sensor by id: {}", sensorId);
+        final Sensor sensor = this.sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(this.sensorMapper.toDto(sensor));
+    }
 
     @PostMapping
     public ResponseEntity<SensorOutput> create(@RequestBody final SensorInput input) {
